7c16e95bbb1f6685ba667b7a60c888f17c61c730
Xavier Wallis <xavierwallis@Xaviers-MacBook-Air.local>
2022-02-11 12:16:10 -0500

    Averia Website thing

--- components/global/navbar.jsx
+++ components/global/navbar.jsx
@@ -1,18 +1,28 @@
 import Image from "next/image"
 import NextLink from 'next/link'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+
 export default function Navbar() {
     return (
-        <nav className={'w-screen shadow-md'}>
-            <div>
-            <NextLink href={'/'}>
-                    <a>
+        <div className={'w-screen shadow-lg align-middle flex p-2 justify-between z-10'}>
+            <div className={'ml-4'}>
+                <NextLink href={'/'}>
+                    <a className={'flex'}>
                         <Image src={'/LogoSmallColor.svg'} width={40} height={40}/>
-                        <span>
+                        <span className={'m-auto font-bold ml-2'}>
                             Averia
                         </span>
                     </a>
                 </NextLink>
             </div>
-        </nav>
+            <div className={'my-auto mr-4 flex'}>
+                <NextLink href={'/getStarted'}>
+                    <a className={'border-b-2 border-[#B304FA] flex px-2'}>
+                        Get Started
+                    </a>
+                </NextLink>
+                <HiOutlineMenuAlt4 />
+            </div>
+        </div>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,29 @@
-import Layout from '../components/global/layout'
-import styles from '../styles/Home.module.css'
+import Wave from "react-wavify"
+import Image from "next/image"
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
-    </div>
+    <>
+      <div className={'bg-white absolute mt-4 ml-6 p-2 rounded-md shadow-lg font-bold'}>
+        <h2>Averia Artificial Intelligence</h2>
+      </div>
+      <div className={'w-screen h-40 bg-[#B304FA]'}>
+        <Image src={'/stockgraphicWhite.svg'} width={400} height={200} className={'absolute z-10 translate-y-2'}/>
+      </div>
+      <Wave className={'rotate-180'} fill={'#B304FA'} paused={false} options={
+          {
+            height:20,
+            amplitude: 20,
+            speed:0.1,
+            points: 5
+          }}/>
+        <div className={'w-screen flex justify-center'}>
+          <div className={'bg-white w-2/3 -translate-y-28 rounded-lg shadow-lg text-center'}>
+            <h2 className={'font-bold my-2 mx-1'}>The most innovative quantitative financial trading architecture.</h2>
+          </div>
+        </div>
+       
+    </>
   )
 }
